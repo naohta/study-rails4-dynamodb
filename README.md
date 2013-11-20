8ceea8ad2250bc1e8e1753c658114bde163e0fad
Naohiro OHTA <naohta@gmail.com>
2013-11-20 16:24:06 +0900

    2nd commit. Created rails app.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 study-rails4-dynamodb
 =====================
+
+https://teachme.jp/contents/30507
+DynamoDBをRuby on Railsで使ってみる
+
+http://rat.cis.k.hosei.ac.jp/article/linux/bash_intro.html
+alias be='bundle exec'
---
+++ my4th/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+My4th::Application.config.secret_key_base = '0d488202fb88f9c82076c9dc22c7d6fb7cea6db67aec9ac8a130a843cce2c85bd8ec7e618af75719d80590bdd52e0751cc65c481f7445ac1bd96952e3ab57e96'
