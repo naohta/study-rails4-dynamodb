65e5c7a5bae392ef1758269a3ee3282b38c275ca
Naohiro OHTA <naohta@gmail.com>
2013-11-20 17:27:48 +0900

    It works! /say/hello -> App write something to dynamodb-table 'rails4-dynamo-sample'

--- my4th/app/controllers/say_controller.rb
+++ my4th/app/controllers/say_controller.rb
@@ -1,4 +1,19 @@
+require 'aws-sdk'
 class SayController < ApplicationController
+  
   def hello
+  	puts "I'm hello method."
+    id = 'AWS_ACCESS_KEY_ID'
+    sec = 'AWS_SECRET_ACCESS_KEY' 
+    puts id + ' ' + ENV[id]
+    puts sec + ' ' + ENV[sec]
+    myTable = AWS::DynamoDB.new(
+      dynamo_db_endpoint:'dynamodb.ap-northeast-1.amazonaws.com',
+      access_key_id:ENV[id],
+      secret_access_key:ENV[sec]
+    ).tables['rails4-dynamodb-sample']
+    myTable.load_schema
+    myTable.items.create(hash:'dave',range:'akita')
   end
+
 end
